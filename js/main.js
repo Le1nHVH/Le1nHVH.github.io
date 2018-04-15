7f608a42e1a1262b5d2fd58b0072b6b2e6f45ac4
Le1n1337 <khvalchev2005@outlook.com>
2018-04-15 12:40:09 +0500

    1
    1

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    var plinks = [
+       	       	{
+            name: 'mein',
+            plink: 'http://steamcommunity.com/id/0x01488'
+        },
+        {
+            name: 'smurf',
+            plink: 'http://steamcommunity.com/id/0x001488'
+
+        },
+		];    
+	
+    for (var i in plinks) {
+        var plink = plinks[i];
+        $('#marquee').append('<a href="' + plink.plink + '" target="_blank">' + plink.name + '</a>');
+        var plink = $('#marquee').children('a').last();
+        if (i != plinks.length - 1) {
+            $('#marquee').append(' - ')
+        }
+    }
+    $('#marquee').marquee({
+        duration: 7000,
+        gap: 310,
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: true
+    })
+
+	window.SetVolume = function(val)
+	{
+    var player = document.getElementById('video');
+    console.log('Before: ' + player.volume);
+    player.volume = val / 100;
+    console.log('After: ' + player.volume);
+	}
+	
+	});
